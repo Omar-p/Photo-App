235b4bfcd44fc0b7fbc03611c866e5b7be1d09f7
Omar Shabaan <omar.plus.077@gmail.com>
2023-11-11 13:54:38 +0200

    apiUsers/inital commit

---
+++ PhotoAppApiUsers/src/test/java/com/example/photoapp/api/users/TestPhotoAppApiUsersApplication.java
@@ -0,0 +1,23 @@
+package com.example.photoapp.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestPhotoAppApiUsersApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(PhotoAppApiUsersApplication::main).with(TestPhotoAppApiUsersApplication.class).run(args);
+	}
+
+}
